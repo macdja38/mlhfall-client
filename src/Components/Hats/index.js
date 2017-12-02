d7cc05d5181a2a67ba8ec28a1e53deaf8d188ee9
bearium <jacob_martin@rogers.com>
2017-12-02 13:31:04 -0500

    added hat buttons

---
+++ src/Components/Hats/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+class Hats extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: ["./santa_hat.png", "top_hat.png"],
+    };
+
+    this.createHats = this.createHats.bind(this);
+
+  }
+
+
+  createHats(img) {
+    return (
+      <button id={"button"}><img src={img} alt="" onClick={() => this.sendHat(img)}/></button>
+    );
+  }
+
+
+  sendHat(src) {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.src = src;
+    });
+  }
+
+  addHat() {
+    console.log("hi")
+    let tgt = document.getElementById('inputImage');
+    let files = tgt.files;
+    // FileReader support
+    if (FileReader && files && files.length) {
+      console.log("hello")
+      let fr = new FileReader();
+      fr.onload = () => {
+        let newList = this.state.list.slice(0);
+        newList.push(fr.result);
+        console.log("h")
+        let newState = Object.assign({}, this.state, {list: newList});
+        this.setState(newState);
+      };
+      fr.readAsDataURL(files[0]);
+    }
+
+
+
+
+  }
+  render() {
+    return (
+      <div>
+      {this.state.list.map(this.createHats)}
+      <input id="inputImage" type="file" name="myImage" accept="image/x-png,image/gif,image/jpeg"/>
+      <button onClick={() => this.addHat()}> submit</button>
+      </div>
+    )
+  }
+
+
+}
+
+export default Hats
